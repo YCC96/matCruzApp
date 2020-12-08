bed7deff0df4251fb312f9edbd1df60c99610373
YCC96 <yordy.cruz_96@outlook.es>
2020-12-07 23:11:35 -0600

    ajustes

--- src/app/components/content-pages/catalogo/catalogo.component.ts
+++ src/app/components/content-pages/catalogo/catalogo.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { environment } from 'src/environments/environment';
-declare var $:any;
+import * as catalogos from '../../../../assets/json/catalogos.json';
+declare var $: any;
 
 @Component({
   selector: 'app-catalogo',
@@ -9,8 +12,72 @@ declare var $:any;
 })
 export class CatalogoComponent implements OnInit {
 
-  constructor() { }
+  title: string;
+  catalogos: any;
+  modal:any = {
+    title: '',
+    img: ''
+  }
 
-  ngOnInit() { }
+  constructor(private _router: Router, private _router1: ActivatedRoute, private _router2: Location) { }
+
+  ngOnInit() {
+    this.getCatalogoUrl();
+  }
+
+  getCatalogoUrl() {
+    var tipo = this._router1.snapshot.paramMap.get("tipo");
+    console.log('*_* catalogo #: ', tipo);
+
+    switch (tipo) {
+      case '1':
+        this.title = 'Catálogo de materiales para construcción';
+        this.catalogos = catalogos.catalogos.construccion;
+
+        break;
+
+      case '2':
+        this.catalogos = catalogos.catalogos.plomeria;
+
+        break;
+
+      case '3':
+        this.catalogos = catalogos.catalogos.electrico;
+
+        break;
+
+      case '4':
+        this.catalogos = catalogos.catalogos.hogar;
+
+        break;
+
+      case '5':
+        this.catalogos = catalogos.catalogos.gas;
+
+        break;
+
+      case '6':
+
+        break;
+
+      default:
+        break;
+    }
+    console.log('*_* catalogo selecionado: ', this.catalogos);
+  }
+
+  abrirImagen(i) {
+    this.modal.title = this.catalogos[i].descripcion;
+    this.modal.img = this.catalogos[i].imagen;
+    $('#modal1').modal('show')
+  }
+
+  regresar() {
+    this._router2.back();
+  }
+
+  hacerPedido() {
+    this._router.navigate(['/pagina', 'comprasTelefono']);
+  }
 
 }
--- src/app/components/content-pages/contactanos/contactanos.component.ts
+++ src/app/components/content-pages/contactanos/contactanos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 declare var $:any;
+//declare var Email:any;
 
 @Component({
   selector: 'app-contactanos',
@@ -29,7 +30,59 @@ export class ContactanosComponent implements OnInit {
   }
 
   enviarMensaje(){
+    /***
+     * 
+     Email.send({
+       Host : "smtp.yourisp.com",
+       Username : "username",
+       Password : "password",
+       To : 'them@website.com',
+       From : "you@isp.com",
+       Subject : "This is the subject",
+       Body : "And this is the body"
+     }).then(
+       message => alert(message)
+     );
+     */
     console.log('Enviar mensaje: ', this.mensaje);
+
+    /**
+     * 
+     * npm install nodemailer --save --verbose
+     * smtpJs
+     console.log("nodeMailerSample()");
+     var nodemailer = require('nodemailer');
+ 
+     console.log("Creating transport...");
+     var transporter = nodemailer.createTransport({
+       host: 'smtp.mycompany.com',
+       port: 25,
+       secure: false, // true for 465, false for other ports
+     });
+ 
+     var mailOptions = {
+       from: 'my.sender.account@mycompany.com',
+       to: 'targetaccount1@othercompany.com,targetaccount2@onemorecompany.com',
+       subject: 'Sending Email using Node.js',
+       text: 'That was easy!'
+     };
+ 
+     console.log("sending email", mailOptions);
+ 
+     transporter.sendMail(mailOptions, function (error, info) {
+       console.log("senMail returned!");
+       if (error) {
+         console.log("ERROR!!!!!!", error);
+       } else {
+         console.log('Email sent: ' + info.response);
+       }
+     });
+ 
+     console.log("End of Script");
+     */
+
+
+
   }
 
 }
